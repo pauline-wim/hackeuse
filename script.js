e19f64fb1e7ea99db9ae668702b136f81015410b
pauline-wim <linette.wim@hotmail.com>
2021-07-12 15:48:43 +0200

    premiere sauvegarde

---
+++ script.js
@@ -0,0 +1,22 @@
+const titreSpans = document.querySelectorAll('h1 .seconde');
+const titreSpansFirst = document.querySelectorAll('h1 .first');
+// const btns = document.querySelectorAll('.btn-first');
+const l1 = document.querySelector('.l1');
+const l2 = document.querySelector('.l2');
+const logo = document.querySelector('.logo');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL.staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: 'power2.out'}, 0.3);
+    TL.staggerFrom(titreSpansFirst, 1, {top: -50, opacity: 0, ease: 'power2.out'}, 0.3, '-=1');
+    // TL.staggerFrom(btns , 1, {opacity: 0, ease: 'power2.out'}, 0.3, '-=1');
+    TL.from(l1, 1, {width: 0, ease: 'power2.out'}, '-=2');
+    TL.from(l2, 1, {width: 0, ease: 'power2.out'}, '-=2');
+    TL.from(logo, 0.4, {transform: 'scale(0)', ease: 'power2.out'}, '-=2');
+    // TL.staggerFrom(btns , 1, {right: -200, ease: 'power2.out'}, 0.3, '-=1');
+
+    TL.play();
+
+})

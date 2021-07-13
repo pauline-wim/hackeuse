9e24874edc3b4998cdc4234765be47e2e87e11ac
pauline-wim <linette.wim@hotmail.com>
2021-07-13 17:49:46 +0200

    menu burger

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ window.addEventListener('load', () => {
     TL.play();
 
 })
+
+//  MENU RESPONSIVE
+function showResponsiveMenu() {
+    var menu = document.getElementById("topnav_responsive_menu");
+    var icon = document.getElementById("topnav_hamburger_icon");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+      menu.className = "open";
+      icon.className = "open";
+      root.style.overflowY = "hidden";
+    } else {
+      menu.className = "";                    
+      icon.className = "";
+      root.style.overflowY = "";
+    }
+  }

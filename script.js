964c4a0c2f961b2a58d4e94257748e6f05343c2d
pauline-wim <linette.wim@hotmail.com>
2021-07-14 15:02:27 +0200

    ajout video popup

--- script.js
+++ script.js
@@ -36,3 +36,49 @@ function showResponsiveMenu() {
       root.style.overflowY = "";
     }
   }
+
+// MODAL - PopUp window
+
+// let modal = null
+
+// const openModal = function (e) {
+//     e.preventDefault()
+//     const targer = document.querySelector(e.target.getAttribute('href'))
+//     target.style.display = null
+//     target.removeAttribute('aria-hidden')
+//     // target.setAttribute('aria-modal', 'true')
+//     modal = target
+//     modal.addEventListener('click', closeModal)
+// }
+
+// const closeModal = function (e) {
+//   if (modal === null) return
+//     e.preventDefault()
+//     modal.style.display = "none"
+//     modal.setAttribute('aria-hidden', 'true')
+//     // modal.removeAttribute('aria-modal')
+//     modal.removeEventListener('click', closeModal)
+//     modal = null;
+// }
+
+//   document.querySelectorAll('.js-modal').forEach(a => {
+//     a.addEventListener('click', openModal)
+//   })
+
+var btnPopup = document.getElementById('btnPopup');
+var modal = document.getElementById('modal');
+var window = document.getElementById('window');
+
+btnPopup.addEventListener('click', openModal);
+modal.addEventListener('click', closeModal);
+window.removeEventListener('click', closeModal);
+
+function openModal(){
+  modal.style.display = 'flex';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+
